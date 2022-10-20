b6aa9be957b7e026147dd1dafd63a7658d616cef
Иван Михайлов <ivan.mikhailov@edu.aspu.ru>
2022-10-20 14:01:49 +0400

    add class Cat and change AppConfig class and change App class and uncomment Test class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = applicationContext.getBean("cat", Cat.class);
+        Cat catBean2 = applicationContext.getBean("cat", Cat.class);
+        System.out.printf("catBean1 (%s) = catBean2 (%s) ? - "
+                + (catBean1 == catBean2), catBean1, catBean2);
+        System.out.println();
+        System.out.printf("HelloWorldBean1 (%s) = HelloWorldBean2 (%s) ? - "
+                + (bean == bean1), bean, bean1);
     }
 }
